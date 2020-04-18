21e5baa5b69427ce72769d290eaba49103c12dc3
PradeepBhadiyadra <pradeepbhadiyadra@gmail.com>
2020-04-18 13:49:55 -0400

    my answer init

--- ans-pg27-2try.sh
+++ ans-pg27-2try.sh
@@ -4,11 +4,15 @@ echo "Enter a Filename"
 read Filename
 #echo "You have entered a \"$Filename\" as variable Filename"
 
-if [ -f ~/$Filename ];
+ansible node01 -m file -a "path=~/$Filename state=file"
+echo $?
+cmdstate=$?
+echo "cmdstate = $cmdstate"
+if [ $cmdstate -eq 0 ];
 then    
     echo "$Filename exist"
     echo "Coping file to node"
-    ansible node01 -m copy -a "remote_src=yes src=~/$Filename dest=~/$Filename.bak"
+    ansible node01 -m copy -a "src=~/$Filename dest=~/$Filename.bak"
 else
     echo "$Filename does not exist"
 fi
--- ans-pradeep/ans-pg27-2try.sh
+++ ans-pradeep/ans-pg27-2try.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+set -e 
+
 echo "Enter a Filename"
 read Filename
 #echo "You have entered a \"$Filename\" as variable Filename"
 
-if [ -f ~/$Filename ];
+ansible node01 -m file -a "path=~/$Filename state=file"
+
+if [$? -eq 0 ];
 then    
     echo "$Filename exist"
     echo "Coping file to node"
-    ansible node01 -m copy -a "remote_src=yes src=~/$Filename dest=~/$Filename.bak"
+    ansible node01 -m copy -a "src=~/$Filename dest=~/$Filename.bak"
 else
     echo "$Filename does not exist"
 fi
