7cf595b6fc1da922bc68240b1a9c2ecde01f86dc
Chandan Kumar <chandank@becloudready.com>
2020-06-07 20:37:10 -0400

    Re organized the repo

--- lab-01/cmd-list.sh
+++
@@ -1,6 +0,0 @@
-for i in `cat host`; do
-ssh -i /Users/chandank/chitra-training-march/id_rsa -oStrictHostKeyChecking=no root@$i apt update -y
-ssh -i /Users/chandank/chitra-training-march/id_rsa -oStrictHostKeyChecking=no root@$i apt install apt-transport-https ca-certificates curl software-properties-common  python -y
-ssh -i /Users/chandank/chitra-training-march/id_rsa -oStrictHostKeyChecking=no root@$i apt update -y
-ssh -i /Users/chandank/chitra-training-march/id_rsa -oStrictHostKeyChecking=no root@$i snap install docker
-done
