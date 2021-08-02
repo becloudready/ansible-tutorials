1fa7a2f6120f056c7d76f68a77ae8a4e5a47af7c
Chandan Kumar <chandank@becloudready.com>
2021-08-02 17:11:03 -0400

    added node01 in adhoc

--- lab-01-adhoc/ad-hoc.sh
+++ lab-01-adhoc/ad-hoc.sh
@@ -6,16 +6,16 @@
 # ansible node01   -a "hostname -f"
 
 # ssh node01 "mkdir /home/ec2-user/mytestdir"
-ansible node00  -a "mkdir /home/ec2-user/testdirAnsible"
-ansible node00 -m copy -a "src=inventory/ansible-nodes dest=/tmp/ansible-nodes"
-ansible node00 -m shell -a "cat /tmp/ansible-nodes" 
-ansible node00 -b -m yum -a "name=httpd state=present"
-ansible node00 -b -m yum -a "name=git state=present"
-ansible node00 -b -m git -a "repo=https://github.com/becloudready/prometheus_monitoring.git dest=/tmp version=HEAD"
+ansible node01  -a "mkdir /home/ec2-user/testdirAnsible"
+ansible node01 -m copy -a "src=inventory/ansible-nodes dest=/tmp/ansible-nodes"
+ansible node01 -m shell -a "cat /tmp/ansible-nodes" 
+ansible node01 -b -m yum -a "name=httpd state=present"
+ansible node01 -b -m yum -a "name=git state=present"
+ansible node01 -b -m git -a "repo=https://github.com/becloudready/prometheus_monitoring.git dest=/tmp version=HEAD"
 
 # Using Ansible modules in ad-hoc commands
-ansible -m -m yum -a "name=nginx state=present" node00 --become
-ansible -m service -a "name=nginx state=started enabled=yes" node00 --become
-ansible -m file -a "path=/home/ec2-user/usingAnsible state=directory" node00 --become
-ansible -m copy -a "src=Vagrantfile dest=/tmp" node00 --become
+ansible -m -m yum -a "name=nginx state=present" node01 --become
+ansible -m service -a "name=nginx state=started enabled=yes" node01 --become
+ansible -m file -a "path=/home/ec2-user/usingAnsible state=directory" node01 --become
+ansible -m copy -a "src=Vagrantfile dest=/tmp" node01 --become
 
