4c174c7aa4dce2e3f82016b5d30ce79975d2362c
Chandan <chandank@live.ca>
2021-08-04 18:15:32 -0400

    Update ad-hoc.sh

--- lab-01-adhoc/ad-hoc.sh
+++ lab-01-adhoc/ad-hoc.sh
@@ -6,7 +6,7 @@
 # ansible node01   -a "hostname -f"
 
 # ssh node01 "mkdir /home/ec2-user/mytestdir"
-ansible node01  -a "mkdir /home/ec2-user/testdirAnsible"
+ansible node01  -a "mkdir -p /home/ec2-user/testdirAnsible"
 ansible node01 -m copy -a "src=inventory/ansible-nodes dest=/tmp/ansible-nodes"
 ansible node01 -m shell -a "cat /tmp/ansible-nodes" 
 ansible node01 -b -m yum -a "name=httpd state=present"
